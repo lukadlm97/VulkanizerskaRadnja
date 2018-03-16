02a5078825e0c26b00101f6188e0c1405cdd3b5d
lukadlm97 <Luka@Luka-PC>
2018-03-16 23:53:21 +0100

    Generisana dokumentacija za kompletan projekat.

---
+++ VulkanizerskaRadnja/doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"gume"},{"l":"gume.radnja"}]
---
+++ VulkanizerskaRadnja/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"gume","l":"AutoGuma"},{"p":"gume","l":"AutoGumaTest"},{"p":"gume.radnja","l":"VulkanizerskaRadnja"},{"p":"gume.radnja","l":"VulkanizerskaRadnjaTest"}]
